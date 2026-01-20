4cc265dcdcf59dd77a9ceeb3bcccad00e97ac1ac
langfeld <48012778+langfeld@users.noreply.github.com>
2026-01-20 17:52:23 +0100

    Fix z-index of the overlay wrapper for improved layering in line highlighting

--- src/index.ts
+++ src/index.ts
@@ -666,7 +666,7 @@ export default class LineHighlightPlugin extends Plugin {
         const totalWidth = rowRect.width + Math.abs(offset) + contentRect.width - 10;
 
         const wrapper = this.createElement('div',
-            { position: 'absolute', left: '-10px', top: '0', width: '0px', height: '100%', pointerEvents: 'none', zIndex: '10', overflow: 'visible' },
+            { position: 'absolute', left: '-10px', top: '0', width: '0px', height: '100%', pointerEvents: 'none', zIndex: '1', overflow: 'visible' },
             { class: 'code-line-highlighter-overlay-wrapper', 'data-code-block-id': this.getCodeBlockId(codeBlock), 'data-hl-spec': spec }
         );
 
